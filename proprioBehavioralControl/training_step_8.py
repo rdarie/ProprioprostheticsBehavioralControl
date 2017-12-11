41d9e7fd666356966b87697efc069430b507d5d7
Radu Darie <radu_darie@brown.edu>
2017-12-11 10:23:51 -0500

    tweaked some rations

--- proprioBehavioralControl/training_step_8.py
+++ proprioBehavioralControl/training_step_8.py
@@ -149,8 +149,8 @@ SM.easyReward = .4
 SM.hardReward = 1.2
 
 #block structure
-SM.smallBlocLength = 2
-SM.bigBlocLength = 6
+SM.smallBlocLength = 6
+SM.bigBlocLength = 4
 
 SM.blocsRemaining = SM.bigBlocLength
 SM.initBlocType = {
