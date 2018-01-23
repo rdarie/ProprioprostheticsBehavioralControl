32414234a9913fb802ed9c3e29e6bf88b1cf56fe
Radu Darie <radu_darie@brown.edu>
2018-01-23 12:08:04 -0500

    tweaked timeout

--- proprioBehavioralControl/training_step_8.py
+++ proprioBehavioralControl/training_step_8.py
@@ -111,7 +111,7 @@ SM = State_Machine()
 # Add attributes to the state machine
 SM.startEnable = False
 SM.nominalTrialLength = float(argTrialLength)
-SM.wrongTimeout = 3
+SM.wrongTimeout = 4
 SM.trialLength = SM.nominalTrialLength
 SM.nextEnableTime = 0
 
