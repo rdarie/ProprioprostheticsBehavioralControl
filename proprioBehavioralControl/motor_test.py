0ff5fd0c2c12c54fd1e553bdac9fa42f9201b388
Radu Darie <radu_darie@brown.edu>
2018-02-01 12:24:32 -0500

    motor test

--- proprioBehavioralControl/motor_test.py
+++ proprioBehavioralControl/motor_test.py
@@ -1,7 +1,12 @@
 import interfaces as ifaces
+import time
 
 motor = ifaces.motorInterface(debugging = True)
+motor.step_size = 10e4
+time.sleep(10)
 
 for i in range(10):
     motor.forward()
+    time.sleep(2)
     motor.backward()
+    time.sleep(2)
