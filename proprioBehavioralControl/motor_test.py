86463022b89ad046e473cc51e569e1cfa4cfd8e7
Radu Darie <radu_darie@brown.edu>
2018-02-04 15:21:40 -0500

    fixed merge

--- proprioBehavioralControl/motor_test.py
+++ proprioBehavioralControl/motor_test.py
@@ -1,17 +1,6 @@
 import interfaces as ifaces
 import time
 
-<<<<<<< HEAD
-motor = ifaces.motorInterface(debugging = True, velocity = 6, acceleration = 300, deceleration = 300)
-motor.step_size = 7e4
-
-for i in range(10):
-    motor.forward()
-    time.sleep(1)
-    motor.backward()
-    time.sleep(1)
-    print('done')
-=======
 motor = ifaces.motorInterface(debugging = True, velocity = 3.3,
     acceleration = 110, deceleration = 110, useEncoder = True)
 motor.step_size = 10e4
@@ -26,4 +15,3 @@ for i in range(10):
         print('Current position = %4.4f' % curPos)
         if curPos < 5 # ~ 2 degrees
             doneMoving = True
->>>>>>> 852eca21edf1874efcd4d68864defb49af7ebda1
--- proprioBehavioralControl/test_motor.py
+++
@@ -1,9 +0,0 @@
-import interfaces as ifaces
-import time
-
-motor = ifaces.motorInterface(debugging = True)
-
-while True:
-	motor.step_size = 5e4
-	motor.forward()
-	time.sleep(5)
