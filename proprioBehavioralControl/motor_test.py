852eca21edf1874efcd4d68864defb49af7ebda1
Radu Darie <radu_darie@brown.edu>
2018-02-04 13:13:59 -0500

    ...

--- proprioBehavioralControl/motor_test.py
+++ proprioBehavioralControl/motor_test.py
@@ -8,12 +8,6 @@ time.sleep(10)
 
 for i in range(10):
     motor.forward()
-    doneMoving = False
-    while not doneMoving:
-        curPos = parent.motor.get_encoder_position()
-        print('Current position = %4.4f' % curPos)
-        if curPos < 5 # ~ 2 degrees
-            doneMoving = True
     motor.backward()
     doneMoving = False
     while not doneMoving:
