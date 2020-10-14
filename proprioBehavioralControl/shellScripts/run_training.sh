6d9aef1461bd06ec2121a8609b367adc5dadc581
Radu Darie <radu_darie@brown.edu>
2020-10-14 12:49:19 -0400

    ...

--- proprioBehavioralControl/shellScripts/run_training.sh
+++ proprioBehavioralControl/shellScripts/run_training.sh
@@ -3,4 +3,4 @@
 # python3 training_with_remote_pi3.py
 # python3 rupert_training_step_1_pi3.py
 # python3 rupert_training_step_2_pi3.py --logLocally True --logToWeb True --playWhiteNoise True
-python3 rupert_training_step_3_pi3.py --logLocally True --logToWeb True --playWhiteNoise True
+python3 rupert_training_step_4_pi3.py --logLocally True --logToWeb True --playWhiteNoise True
