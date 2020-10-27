66cf9c279629e44fb5a24a533151cc9e9babe5cb
Radu Darie <radu_darie@brown.edu>
2020-10-27 13:26:55 -0400

    folding minirc into python code

--- proprioBehavioralControl/shellScripts/run_training.sh
+++ proprioBehavioralControl/shellScripts/run_training.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # python3 training_with_remote_pi3.py
-# python3 rupert_training_step_1_pi3.py
 # python3 rupert_training_step_2_pi3.py --logLocally True --logToWeb True --playWhiteNoise True
-python3 rupert_training_step_4_pi3.py --logLocally True --logToWeb True --playWhiteNoise True
+python3 rupert_training_minirc_step_1_pi3.py --logLocally True --logToWeb True --playWhiteNoise True
